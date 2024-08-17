bfc61cbfb2c5c77ea71ea92a6e0dc40e504cfcc5
koushikballa006 <koushikballa06@gmail.com>
2024-08-17 16:52:37 +0530

    headersectionupdate

--- src/components/headersection.tsx
+++ src/components/headersection.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import RetroGrid from "../components/magicui/retro-grid"; // Adjust path if needed
 import WordPullUp from "../components/magicui/word-pull-up"; // Adjust path if needed
+import NumberTicker from "../components/magicui/number-ticker"; // Adjust path if needed
 
 const HeaderSection: React.FC = () => {
   return (
@@ -9,16 +10,21 @@ const HeaderSection: React.FC = () => {
       <RetroGrid className="absolute inset-0 z-0" angle={65} />
 
       {/* Content */}
-      <div className="relative z-10 flex items-center justify-start h-full w-full px-10 mt-[-150px]">
+      <div className="relative z-10 flex items-center justify-start h-full w-full px-10 mt-[-60px]">
         <div className="flex flex-col justify-center items-start h-full">
           <WordPullUp
             words="Karthik Academy"
             className="text-7xl font-bold text-primary leading-tight"
           />
-          {/* Additional text */}
-          <p className="mt-4 text-xl font-medium text-primary">
-            Over 10,000 students have successfully graduated from our academy.
-          </p>
+          {/* Additional text with NumberTicker */}
+          <div className="mt-4 text-xl font-medium text-primary">
+            <p className="text-xl font-medium text-primary">
+              With over <NumberTicker value={10000} /> +successful graduates,
+              our academy stands as a <br></br>beacon of excellence, reflecting
+              our commitment to delivering superior <br></br>education and
+              fostering academic success.
+            </p>
+          </div>
         </div>
       </div>
     </header>
